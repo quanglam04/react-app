b0c67ca276b6da7e730ba6b9080d2be87044acbb
quanglam04 <trinhquanglam2k4@gmail.com>
2025-01-06 22:49:23 +0700

    truyền data từ component cha sang con

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,23 @@ import reactLogo from './assets/react.svg'
 
 const App = () => {
 
-
+  const test = "Trinh Quang Lam"
+  const test_2 = 25
+  const data = {
+    address: "hanoi",
+    country: "vietnam"
+  }
   return (
     <div className="todo-container">
       <div className="todo-title">
         Todo List
       </div>
       <TodoNew />
-      <TodoData />
+      <TodoData
+        name={test}
+        test_2={test_2}
+        data={data}
+      />
       <div className='todo-images'>
         <img src={reactLogo} className="logo react" />
       </div>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,6 +1,10 @@
-const TodoData = () => {
+/* eslint-disable react/prop-types */
+// Phải để là props để react nhận diện data truyền từ component cha
+const TodoData = (props) => {
+    console.log(">>>>>>>", props)
     return (
         <div className='todo-data'>
+            <div>My name is {props.name}</div>
             <div>Learning React</div>
             <div>Watching Youtobe</div>
         </div>
