f263b689e81f1e57c29e81a46c51efc0ff695b01
quanglam04 <trinhquanglam2k4@gmail.com>
2025-01-06 22:26:57 +0700

    chia layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,26 @@
 /* eslint-disable no-unused-vars */
-import { useState } from 'react'
+import TodoData from './components/todo/TodoData'
+import TodoNew from './components/todo/TodoNew'
+import './components/todo/todo.css'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import MyComponent from './components/learn/MyComponent'
-import {SecondComponent, ThirdComponent} from './components/learn/SecondComponent'
-//component = html + css + js
-
 
 const App = () => {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="todo-container">
+      <div className="todo-title">
+        Todo List
       </div>
-      <h1>Vite + React</h1>
-      <MyComponent></MyComponent>
-      <MyComponent/>
-      <SecondComponent/>
-      <ThirdComponent/>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <TodoNew />
+      <TodoData />
+      <div className='todo-images'>
+        <img src={reactLogo} className="logo react" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-// function App() {
-//   const [count, setCount] = useState(0)
 
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
 
 export default App
---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+const TodoNew = () => {
+    return (
+        <div className='todo-new'>
+            <input type="text" />
+            <button>Add</button>
+        </div>
+    )
+}
+export default TodoNew;
