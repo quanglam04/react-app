af6d8a813e206c5763fa4843ce99573b83b9fe5c
quanglam04 <trinhquanglam2k4@gmail.com>
2025-01-07 15:13:34 +0700

    client Route với Link

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,12 +1,11 @@
-import Footer from "../components/layout/footer/footer"
-import Header from "../components/layout/header/header"
+
 
 const ProductPage = () => {
     return (
         <>
-            <Header />
+
             <div>products page</div>
-            <Footer />
+
         </>
     )
 }
--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -1,12 +1,10 @@
-import Footer from "../components/layout/footer/footer"
-import Header from "../components/layout/header/header"
 
 const UserPage = () => {
     return (
         <>
-            <Header />
+
             <div>users page</div>
-            <Footer />
+
         </>
     )
 }
