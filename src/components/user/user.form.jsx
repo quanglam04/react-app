ade2b3247bb921f3afd080e0d16d1b4bca8264e2
quanglam04 <trinhquanglam2k4@gmail.com>
2025-01-07 21:21:49 +0700

    state hóa form

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,31 +1,45 @@
+/* eslint-disable no-unused-vars */
 import { Button, Input } from "antd"
+import { useState } from "react"
 
 const UserForm = () => {
+
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("")
+    const [passWord, setPassWord] = useState("")
+    const [phoneNumber, setPhoneNumber] = useState("")
+
+    const handleClick = () => {
+        console.log(fullName, email, passWord, phoneNumber)
+    }
     return (
         <div className="user-form" style={{ margin: "20px 0" }}>
             <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
                 <div>
                     <span>FullName</span>
-                    <Input />
+                    <Input onChange={(event) => setFullName(event.target.value)}
+                    />
                 </div>
 
                 <div>
                     <span>Email</span>
-                    <Input />
+                    <Input onChange={(event) => setEmail(event.target.value)} />
                 </div>
 
                 <div>
                     <span>Password</span>
-                    <Input.Password />
+                    <Input.Password onChange={(event) => setPassWord(event.target.value)} />
                 </div>
 
                 <div>
                     <span>PhoneNumber</span>
-                    <Input />
+                    <Input onChange={(event) => setPhoneNumber(event.target.value)} />
                 </div>
 
                 <div>
-                    <Button type="primary"> Create User</Button>
+                    <Button type="primary"
+                        onClick={() => handleClick()}
+                    > Create User</Button>
                 </div>
             </div>
         </div>
