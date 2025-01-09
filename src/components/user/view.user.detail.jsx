3ff67373db78304ff4bd9210d808e5e73444db5b
quanglam04 <trinhquanglam2k4@gmail.com>
2025-01-09 14:48:42 +0700

    hiển thị avatar use

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -11,12 +11,12 @@ const ViewUserDetail = (props) => {
     return (
         <>
 
-            <Drawer title="View Detail" onClose={() => {
-                setDataViewDetail(null)
-                setIsOpenViewDetail(false)
-
-
-            }} open={isOpenViewDetail}>
+            <Drawer
+                width={"40vw"}
+                title="View Detail" onClose={() => {
+                    setDataViewDetail(null)
+                    setIsOpenViewDetail(false)
+                }} open={isOpenViewDetail}>
                 {
                     dataViewDetail ?
                         <>
@@ -26,7 +26,30 @@ const ViewUserDetail = (props) => {
                             <br></br>
                             <h3>Email: {dataViewDetail.email}</h3>
                             <br></br>
-                            <h3>Phone: {dataViewDetail.phone}</h3></>
+                            <h3>Phone: {dataViewDetail.phone}</h3>
+                            <br></br>
+                            <h3>Avatar:</h3>
+                            <br></br>
+                            <div>
+                                <img
+                                    height={100} width={150}
+                                    src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataViewDetail.avatar}`} />
+                            </div>
+                            <div>
+                                <label htmlFor="btnUpload"
+
+                                    style={{
+                                        display: "block", width: "fit-content", marginTop: "15px", padding: "5px 10px"
+                                        , background: "orange", borderRadius: "5px", cursor: "pointer"
+
+                                    }}>Upload File</label>
+                                <input type="file" hidden id="btnUpload"></input>
+                            </div>
+
+                            {/* <Button type="primary" >Upload File</Button> */}
+
+
+                        </>
                         : <>
                             <p>Không có dữ liệu</p>
                         </>
