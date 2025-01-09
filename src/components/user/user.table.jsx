3abf57b1ecc01f8540de6efd8502d1e364063a99
quanglam04 <trinhquanglam2k4@gmail.com>
2025-01-09 10:25:31 +0700

    setup eslint

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
+
 import { message, notification, Popconfirm, Space, Table, Tag } from 'antd';
 import { deleteUserAPI, fetchAllUserAPI } from '../../services/api.service';
 import { useEffect, useState } from 'react';
