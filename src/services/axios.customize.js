b1d2ac402198b8999162e2dcff61210a34d91c42
quanglam04 <trinhquanglam2k4@gmail.com>
2025-01-18 22:29:37 +0700

    loading bar

--- src/services/axios.customize.js
+++ src/services/axios.customize.js
@@ -1,16 +1,26 @@
 import axios from "axios";
 
+
+import NProgress from 'nprogress';
+
+NProgress.configure({
+    showSpinner: true,
+    trickleSpeed: 100,
+});
+
 const instance = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL
 })
 
 instance.interceptors.request.use(function (config) {
+    NProgress.start()
     if (typeof window !== "undefined" && window && window.localStorage && window.localStorage.getItem('access_token')) {
         config.headers.Authorization = 'Bearer ' + window.localStorage.getItem('access_token');
     }
     // Do something before request is sent
     return config;
 }, function (error) {
+    NProgress.done()
     // Do something with request error
     return Promise.reject(error);
 });
@@ -18,12 +28,14 @@ instance.interceptors.request.use(function (config) {
 
 // Add a response interceptor
 instance.interceptors.response.use(function (response) {
+    NProgress.done()
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     if (response.data && response.data.data) return response.data
     return response
 
 }, function (error) {
+    NProgress.done()
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     if (error.response && error.response.data) return error.response.data
